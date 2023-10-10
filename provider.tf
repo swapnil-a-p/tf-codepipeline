6ef94973b5f0d3c6835d45965a78f5709ef0c509
swapnilp <40246933+swapnil-a-p@users.noreply.github.com>
2023-10-10 15:59:27 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+backend "s3" {
+  bucket = "tf-cp-swapanch"
+  key = "backed"
+  region = "us-east-1"
 }
-
+}
 provider "aws" {
   region = "us-east-1"
 }
